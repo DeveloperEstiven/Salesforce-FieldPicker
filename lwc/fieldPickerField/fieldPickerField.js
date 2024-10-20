0e8d4107b2c14769b1f0f33ce371bb94f249b35c
DeveloperEstiven <hundsomeluke@gmail.com>
2024-10-20 17:11:48 +0300

    feature/highlight-search

--- lwc/fieldPickerField/fieldPickerField.js
+++ lwc/fieldPickerField/fieldPickerField.js
@@ -1,11 +1,38 @@
 import { LightningElement, api } from "lwc";
 
+//TODO: /** @type {Field} */
 export default class FieldPickerField extends LightningElement {
-    //TODO: /** @type {Field} */
     @api field;
     @api isDisabled = false;
     @api actionsDisabled = false;
     @api actions = [];
+    @api searchTerm = "";
+
+    get labelParts() {
+        const label = this.field.label || "";
+        const searchTerm = this.searchTerm.toLowerCase();
+
+        if (!searchTerm) {
+            return [{ text: label, class: "" }];
+        }
+
+        const lowerLabel = label.toLowerCase();
+        const startIndex = lowerLabel.indexOf(searchTerm);
+
+        if (startIndex === -1) {
+            return [{ text: label, class: "" }];
+        }
+
+        const beforeMatch = label.substring(0, startIndex);
+        const match = label.substring(startIndex, startIndex + searchTerm.length);
+        const afterMatch = label.substring(startIndex + searchTerm.length);
+
+        return [
+            { text: beforeMatch, class: "" },
+            { text: match, class: "highlight" },
+            { text: afterMatch, class: "" }
+        ];
+    }
 
     get actionsVisible() {
         return this.actions.length && !this.actionsDisabled;
